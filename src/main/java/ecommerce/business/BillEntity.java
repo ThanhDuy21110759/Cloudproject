969e5f4af4cfbf82f135dc249d26dad7b3b20514
Thanh Duy <145462659+ThanhDuy21110759@users.noreply.github.com>
2023-12-17 18:48:02 +0700

    Update BillEntity.java

--- src/main/java/ecommerce/business/BillEntity.java
+++ src/main/java/ecommerce/business/BillEntity.java
@@ -2,6 +2,7 @@ package ecommerce.business;
 
 import ecommerce.data.CustomerDB;
 import ecommerce.data.ProductDB;
+import java.io.Serializable;
 import jakarta.persistence.*;
 import java.util.List;
 import java.sql.Timestamp;
